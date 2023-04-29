18df72b689cf7fededb874ff074fc9899170cf61
led-dotcom <74643143+led-dotcom@users.noreply.github.com>
2023-04-28 23:33:04 -0400

    feat: add icon image

---
+++ src/app/components/Icon.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import L from 'leaflet'
+const marker = '/icon.png'
+
+const myIcon = new L.Icon({
+  iconUrl: marker,
+  iconRetinaUrl: marker,
+  popupAnchor: [-0, -0],
+  iconSize: [32, 45]
+})
+
+export default myIcon
--- src/app/components/Map.tsx
+++ src/app/components/Map.tsx
@@ -1,16 +1,28 @@
 'use client'
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+
+import Icon from './Icon'
+
+// * ts types
+import { LatLngTuple } from 'leaflet'
+
+const position: LatLngTuple = [51.505, -0.09]
 
 const Map = () => {
   return (
     <div>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer
+        center={position}
+        zoom={13}
+        scrollWheelZoom={true}
+        style={{ height: '500px' }}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
+        <Marker position={position} icon={Icon}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
