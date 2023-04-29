41836aa77e13b74bb0e78fd09790376360f951fb
led-dotcom <74643143+led-dotcom@users.noreply.github.com>
2023-04-28 22:14:15 -0400

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Map from "./components/Map";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="font-sans text-mammoth font-bold ">Main Page</h1>
+      <Map />
+    </div>
+  );
+}
---
+++ src/app/testpage/subpage1/page.tsx
@@ -0,0 +1,10 @@
+const Subpage1 = () => {
+  return (
+    <div>
+      <h1>Test Page</h1>
+      <h2>Sub Page1</h2>
+    </div>
+  )
+}
+
+export default Subpage1
