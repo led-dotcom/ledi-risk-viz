e83475757508e2a58aff75806c4680c8385d8e23
led-dotcom <74643143+led-dotcom@users.noreply.github.com>
2023-05-04 09:44:37 -0400

    feat: marker cluster

--- src/app/components/Map.tsx
+++ src/app/components/Map.tsx
@@ -1,8 +1,11 @@
 'use client'
 
 import 'leaflet/dist/leaflet.css'
+import 'leaflet.markercluster/dist/MarkerCluster.css'
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import MarkerClusterGroup from './MarkerClusterGroup'
 import Icon from './Icon'
 
 // * ts types
@@ -13,7 +16,11 @@ const positions: LatLngTuple[] = [
   [51.51, -0.05],
   [51.51, -0.1],
   [51.51, -0.12],
-  [51.505, -0.09]
+  [51.505, -0.09],
+  [51.505, -0.08],
+  [51.505, -0.07],
+  [51.505, -0.06],
+  [51.505, -0.05]
 ]
 
 const position: LatLngTuple = [51.505, -0.09]
@@ -30,13 +37,15 @@ const Map = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {positions.map((position, index) => (
-          <Marker key={index} position={position} icon={Icon}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        ))}
+        <MarkerClusterGroup>
+          {positions.map((position, index) => (
+            <Marker key={index} position={position} icon={Icon}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          ))}
+        </MarkerClusterGroup>
       </MapContainer>
     </div>
   )
